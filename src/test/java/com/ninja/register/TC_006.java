f2a27083263b0a8140eea556460ee4366ccabeeb
GURU080 <guruprasadm1502@gmail.com>
2025-07-21 17:43:37 +0530

    today AGAIN

---
+++ src/test/java/com/ninja/register/TC_006.java
@@ -0,0 +1,68 @@
+package com.ninja.register;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import com.ninja.utils.EmailUtils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TC_006 {
+	
+	
+     @Test
+	public void verifyregisterwithnonewsletter () {
+
+		WebDriverManager.chromedriver().setup();
+	    WebDriver driver = new ChromeDriver();
+	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+	    driver.manage().window().maximize();
+	    driver.get("https://tutorialsninja.com/demo/");
+
+	    driver.findElement(By.xpath("//span[text()='My Account']")).click();
+	    driver.findElement(By.xpath("//a[text()='Register']")).click();
+	    driver.findElement(By.id("input-firstname")).sendKeys("REVANT");
+	    driver.findElement(By.id("input-lastname")).sendKeys("SHAR");
+
+	    driver.findElement(By.id("input-email")).sendKeys(EmailUtils.generateUniqueEmail());
+	    driver.findElement(By.id("input-telephone")).sendKeys("879876696");
+	    driver.findElement(By.id("input-password")).sendKeys("REV@123");
+	    driver.findElement(By.id("input-confirm")).sendKeys("REV@123");
+	    
+	    
+	    
+    driver.findElement(By.xpath("//label[normalize-space()='Yes']//input[@name='newsletter']")).click();
+
+   driver.findElement(By.name("agree")).click();
+	    driver.findElement(By.xpath("//input[@value='Continue']")).click();
+//	    
+//
+//	    driver.findElement(By.xpath("//*[@id=\"content\"]/div/div/a")).click();
+//	    
+//	    
+//	  
+//	    
+//	    driver.findElement(By.xpath("//*[@id=\"content\"]/ul[4]/li/a")).click();
+//	    
+//	    
+//	    driver.findElement(By.xpath("//*[@id=\"content\"]/form/div/div[2]/input")).click();
+	    
+	  
+	  
+	
+	    
+//	    String ep="Success: Your newsletter subscription has been successfully updated!";
+//	    
+//	    
+//	    Assert.assertEquals(driver.findElement(By.xpath("//*[@id=\"account-account\"]/div[1]/text()")).getText(), ep);
+	    
+//	    driver.quit();
+	    
+
+	}
+
+}
